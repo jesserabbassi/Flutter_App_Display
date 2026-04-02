40bcc6d6063342dbbd5a300a25e4e073aa4daac2
Jesser Abbassi <jesserabbassi@gmail.com>
2026-04-02 10:18:17 +0100

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WebAppScreen(),
+    );
+  }
+}
+
+class WebAppScreen extends StatefulWidget {
+  const WebAppScreen({super.key});
+
+  @override
+  State<WebAppScreen> createState() => _WebAppScreenState();
+}
+
+class _WebAppScreenState extends State<WebAppScreen> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse('https://ooredoo-app-1.onrender.com/'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('My App')),
+      body: WebViewWidget(controller: controller),
+    );
+  }
+}
